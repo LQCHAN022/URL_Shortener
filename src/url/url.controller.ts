190de91cc0dbf08d0b05f31aa50233361511b5e9
unknown <lqchan022@gmail.com>
2023-02-16 19:48:39 +0900

    Implement post routing to shorten url

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { 
+        Controller, 
+        Body, 
+        Get, 
+        Param,
+        Post,
+        Res 
+} from '@nestjs/common';
+import { UrlService } from './url.service';
 
-@Controller('url')
-export class UrlController {}
+@Controller() // "url" removed to handle shortened links (that does not have /url/ in route)
+export class UrlController {
+    constructor(
+        private service: UrlService
+    ) {}
+}

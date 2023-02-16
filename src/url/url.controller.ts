9621148e795392da38b16251f0d98d61fd2182e9
unknown <lqchan022@gmail.com>
2023-02-16 19:53:51 +0900

    Implement get request to redirect

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -6,6 +6,7 @@ import {
         Post,
         Res 
 } from '@nestjs/common';
+import { ShortenURLDto } from './dtos/url.dto';
 import { UrlService } from './url.service';
 
 @Controller() // "url" removed to handle shortened links (that does not have /url/ in route)
@@ -13,4 +14,22 @@ export class UrlController {
     constructor(
         private service: UrlService
     ) {}
+
+    @Post("shorten")
+    shortenUrl(
+        @Body() url: ShortenURLDto,
+    ) {
+        return this.service.shortenUrl(url);
+    }
+
+    @Get(":code")
+    async redirect(
+        @Res() res,
+        @Param("code") code: string,
+    ) {
+        const url = await this.service.redirect(code);
+
+        return res.redirect(url.longUrl)
+    }
+
 }

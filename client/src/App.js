950824a27090bb9e2c7ac9170f3e98468492ea92
unknown <lqchan022@gmail.com>
2023-02-17 13:32:00 +0900

    Implement shortenUrl fetch function

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,42 @@
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
   return (
     <div className="App">
       <h1>URL Shortener</h1>
-      <input className="UserInput"></input>
-      <button className="SubmitButton">Shorten URL</button>
-      <p className="GeneratedURL">Generated URL</p>
+      <input id="user_input"></input>
+      <button id="submit_btn" onClick={() => shortenURL(setUrl)}>
+        Shorten URL
+      </button>
+      <p id="shortened_url">
+        <a href={url}>{url}</a>
+      </p>
     </div>
   );
 }
 
 export default App;
 
-async function ShortenURL(url) {
-  let response = await fetch(
-    "http://localhost:3000/shorten", 
-    {
-      method: "POST",
-      body: {longUrl: url},
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-  );
-
-  const resJson = response.json();
+function shortenURL(setUrl) {
+  let user_input = document.getElementById('user_input').value;
+  // console.log(user_input);
+  // fetch('http://localhost:3000/sanity_check', {
+  fetch('http://localhost:3000/shorten', {
+    method: 'POST',
+    // headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      longUrl: user_input,
+    }),
+  })
+    .then((res) => {
+      // console.log(res);
+      return res.text();
+    })
+    .then((text) => {
+      // console.log(text);
+      setUrl(text);
+    });
 }

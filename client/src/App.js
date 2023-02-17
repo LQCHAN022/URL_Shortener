51d82a0f64cb37d0215cbd3031a4a78f72760bf4
unknown <lqchan022@gmail.com>
2023-02-17 13:27:49 +0900

    Implement shortenUrl fetch function

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>URL Shortener</h1>
+      <input className="UserInput"></input>
+      <button className="SubmitButton">Shorten URL</button>
+      <p className="GeneratedURL">Generated URL</p>
     </div>
   );
 }
 
 export default App;
+
+async function ShortenURL(url) {
+  let response = await fetch(
+    "http://localhost:3000/shorten", 
+    {
+      method: "POST",
+      body: {longUrl: url},
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  );
+
+  const resJson = response.json();
+}
